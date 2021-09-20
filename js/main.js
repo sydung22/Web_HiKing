82ff3786d95531975191295633e63b3be7d03231
sydung22 <72313449+sydung22@users.noreply.github.com>
2021-09-20 17:11:54 -0600

    add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
 
+    window.addEventListener('scroll', function(){
+        let pageTop = document.querySelector('#btnScrolltoTop');
+        let windowPosition = window.scrollY > 192;
+        pageTop.classList.toggle('active', windowPosition);
+      })
+      
+      const btnScrolltoTop = document.querySelector('#btnScrolltoTop');
+      btnScrolltoTop.addEventListener('click', function(){
+        $('html, body').animate({scrollTop: 0}, 'slow');
+      });
+
 });
