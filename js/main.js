1bd4b73b27fae51e80e013f3f1727d79dfe1947c
sydung22 <72313449+sydung22@users.noreply.github.com>
2021-09-21 12:28:31 -0600

    add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $( document ).ready(function() {
 
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    arrows: false,
+    autoplaySpeed: 3000,
+    speed: 1500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
     window.addEventListener('scroll', function(){
         let pageTop = document.querySelector('#btnScrolltoTop');
         let windowPosition = window.scrollY > 192;
@@ -9,6 +22,19 @@ $( document ).ready(function() {
       const btnScrolltoTop = document.querySelector('#btnScrolltoTop');
       btnScrolltoTop.addEventListener('click', function(){
         $('html, body').animate({scrollTop: 0}, 'slow');
+      });
+
+      $(".col-img").slice(0, 3).show()
+      $("#load-more").on('click', function(e){
+          e.preventDefault();
+          $(".col-img:hidden").slice(0, 3).slideDown()
+          if($(".col-img:hidden").length == 0){
+            $("#load-more").fadeOut('slow')
+          }
+      }) 
+
+      $('.carousel').carousel({
+          interval: 3500 * 1
       });
 
 });
